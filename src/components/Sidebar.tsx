d9037145c995e5a6c46ad01a3f21f2f9b473f443
Gurvir Singh <gurvirbaraich@gmail.com>
2023-10-29 17:05:33 +0530

    updated

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,11 +12,18 @@ const images = [
     alt: "Customers Icon",
     src: "/svgs/customers.icon.svg",
   },
+  {
+    link: "/services",
+    alt: "Services Icon",
+    src: "/svgs/services.icon.svg",
+    scale: 2,
+    activeSrc: "/svgs/services-light.icon.svg",
+  },
 ];
 
 export default function Sidebar() {
   return (
-    <div className="h-full min-h-screen bg-white flex flex-col justify-center items-center border-r">
+    <div className="h-full min-h-screen bg-white gap-2 flex flex-col justify-center items-center border-r">
       {images.map((image, _) => (
         <div
           key={image.alt}
@@ -25,21 +32,22 @@ export default function Sidebar() {
             "flex cursor-pointer items-center justify-center w-14 h-14 " +
             (location.pathname == image.link ||
             (location.pathname == "/" && _ == 0)
-              ? "bg-[#383838] rounded-2xl"
+              ? "bg-[#434343] rounded-2xl"
               : "")
           }
         >
-          <img
-            width={24}
-            height={24}
-            src={
-              location.pathname == image.link ||
-              (location.pathname == "/" && _ == 0)
-                ? image.activeSrc ?? image.src
-                : image.src
-            }
-            alt={image.alt}
-          />
+          <div className="w-6 h-6 overflow-hidden grid place-items-center">
+            <img
+              style={{ transform: `scale(${image.scale})` }}
+              src={
+                location.pathname == image.link ||
+                (location.pathname == "/" && _ == 0)
+                  ? image.activeSrc ?? image.src
+                  : image.src
+              }
+              alt={image.alt}
+            />
+          </div>
         </div>
       ))}
     </div>
--- src/routes/config.ts
+++ src/routes/config.ts
@@ -11,4 +11,8 @@ export const routes = [
     name: "Customers",
     path: "/customers",
   },
+  {
+    name: "Services",
+    path: "/services",
+  },
 ];
