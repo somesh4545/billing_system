3124bbb25935a3533b44b6f7c0f6ec1927788435
Gurvir Singh <gurvirbaraich@gmail.com>
2023-10-31 23:27:28 +0530

    fixed contactFieldset bug

--- src/components/ContactFieldset.tsx
+++ src/components/ContactFieldset.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   setAddingAnotherAddress,
@@ -11,7 +11,7 @@ export default function ContactFieldset() {
   const contactIDRef = useRef<number>();
   const { customerSelected } = useSelector((state: any) => state.customers);
 
-  console.log(customerSelected);
+  const [contacts, setContacts] = useState([]);
 
   function addContact() {
     dispatch(setAddingAnotherAddress(false));
@@ -22,6 +22,81 @@ export default function ContactFieldset() {
     dispatch(setEditingContactID(value));
   }
 
+  useEffect(function () {
+    const contactsComputed = customerSelected.map((customer, _) => {
+      if (
+        contactIDRef.current != customer.ContactID &&
+        customer.ContactID != null
+      ) {
+        contactIDRef.current = customer.ContactID;
+
+        return (
+          <details
+            className="cursor-pointer border-b"
+            key={`${customer.ContactID}_${customer.ContactName}`}
+          >
+            <summary className="flex gap-2 border-b pb-1">
+              <input type="checkbox" name="" id="" />
+              <span>{customer.ContactName}</span>
+            </summary>
+
+            <div className="py-2 grid">
+              <div className="grid grid-cols-2 grid-rows-1 gap-2 mt-2">
+                <div className="text-[#888] text-sm">Contact Name</div>
+                <div className="text-right flex gap-2 justify-end">
+                  <span className="break-all">{customer.ContactName}</span>
+                  <img
+                    src="/svgs/pencil.icon.svg"
+                    alt="Pencil Icon"
+                    onClick={() =>
+                      setEditingContactIDValue({
+                        index: _,
+                        AddressID: customer.AddressID,
+                      })
+                    }
+                  />
+                </div>
+              </div>
+              <div className="flex gap-2 justify-between relative">
+                <div className="w-full">
+                  <div className="grid grid-cols-2 grid-rows-1 gap-2 mt-2">
+                    <div className="text-[#888] text-sm">Contact Phone No.</div>
+                    <div className="text-right">
+                      +{customer.ContactPhonePrefix} {customer.ContactPhone}
+                    </div>
+                  </div>
+                  <div
+                    className="absolute right-0"
+                    style={{
+                      top: 16,
+                    }}
+                  >
+                    <img
+                      src="/images/addressLink.png"
+                      width={24}
+                      height={34}
+                      alt="Address Link"
+                      className="object-contain addressLink"
+                    />
+                  </div>
+                  <div className="grid grid-cols-2 grid-rows-1 gap-2 mt-2">
+                    <div className="text-[#888] text-sm">Contact Address</div>
+                    <div className="text-right">
+                      {customer.CompanyStreetAddress}
+                    </div>
+                  </div>
+                </div>
+                <div className="w-5"></div>
+              </div>
+            </div>
+          </details>
+        );
+      }
+    });
+
+    setContacts(contactsComputed);
+  }, []);
+
   return (
     <fieldset className="border p-4 pt-2 rounded max-h-[290px] border-gray-300 overflow-y-hidden">
       <legend>
@@ -29,80 +104,7 @@ export default function ContactFieldset() {
       </legend>
       <div className="bg-white rounded pt-3 p-4 h-full shadow-sm grid grid-rows-[1fr_auto] overflow-scroll">
         <div className="h-full flex gap-2 flex-col">
-          {customerSelected.map((customer, _) => {
-            if (contactIDRef.current != customer.ContactID && customer.ContactID != null) {
-              contactIDRef.current = customer.ContactID;
-
-              return (
-                <details
-                  className="cursor-pointer border-b"
-                  key={`${customer.ContactID}_${customer.ContactName}`}
-                >
-                  <summary className="flex gap-2 border-b pb-1">
-                    <input type="checkbox" name="" id="" />
-                    <span>{customer.ContactName}</span>
-                  </summary>
-
-                  <div className="py-2 grid">
-                    <div className="grid grid-cols-2 grid-rows-1 gap-2 mt-2">
-                      <div className="text-[#888] text-sm">Contact Name</div>
-                      <div className="text-right flex gap-2 justify-end">
-                        <span className="break-all">
-                          {customer.ContactName}
-                        </span>
-                        <img
-                          src="/svgs/pencil.icon.svg"
-                          alt="Pencil Icon"
-                          onClick={() =>
-                            setEditingContactIDValue({
-                              index: _,
-                              AddressID: customer.AddressID,
-                            })
-                          }
-                        />
-                      </div>
-                    </div>
-                    <div className="flex gap-2 justify-between relative">
-                      <div className="w-full">
-                        <div className="grid grid-cols-2 grid-rows-1 gap-2 mt-2">
-                          <div className="text-[#888] text-sm">
-                            Contact Phone No.
-                          </div>
-                          <div className="text-right">
-                            +{customer.ContactPhonePrefix}{" "}
-                            {customer.ContactPhone}
-                          </div>
-                        </div>
-                        <div
-                          className="absolute right-0"
-                          style={{
-                            top: 16,
-                          }}
-                        >
-                          <img
-                            src="/images/addressLink.png"
-                            width={24}
-                            height={34}
-                            alt="Address Link"
-                            className="object-contain addressLink"
-                          />
-                        </div>
-                        <div className="grid grid-cols-2 grid-rows-1 gap-2 mt-2">
-                          <div className="text-[#888] text-sm">
-                            Contact Address
-                          </div>
-                          <div className="text-right">
-                            {customer.CompanyStreetAddress}
-                          </div>
-                        </div>
-                      </div>
-                      <div className="w-5"></div>
-                    </div>
-                  </div>
-                </details>
-              );
-            }
-          })}
+          {contacts.map((contact) => contact)}
         </div>
 
         <div className="flex justify-end h-max mt-2">
